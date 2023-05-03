9c8db1a108ea19c85d8ad5f1606bc347edfac10c
eazyseon <eazyseon@gmail.com>
2023-05-03 17:25:49 +0900

    feat : google 로그인 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <div className="w-full max-w-xl mx-auto">
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
     </div>
   );
 }
